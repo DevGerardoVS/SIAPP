1409a119d006ff8cbc888ca458741d9d0ee60c7f
zavalaroses <zavalaroses@gmail.com>
2023-06-08 11:51:26 -0600

    addRoutes

--- routes/calendarizacion.php
+++ routes/calendarizacion.php
@@ -1,6 +1,17 @@
 <?php 	
 
     use App\Http\Controllers\Calendarizacion\ClavePreController;
+	use App\Http\Controllers\Calendarización\MetasController;
+	use App\Http\Controllers\Calendarización\TechosController;
+	Route::controller(MetasController::class)->group(function () {
+		Route::get('/calendarizacion/metas', 'getIndex')->name('index_metas');
+	 	Route::get('/calendarizacion/data', 'getMetas')->name('getMetas');
+        Route::get('/calendarizacion/proyecto', 'getProyecto')->name('proyecto');
+	});
+
+    Route::controller(TechosController::class)->group(function () {
+        Route::get('/calendarizacion/techos', 'getIndex')->name('index_techos');
+    });
 	Route::controller(ClavePreController::class)->group(function () {
 		Route::get('/calendarizacion/claves', 'getPanel');
 		Route::get('/calendarizacion/claves-get', 'getClaves');
