4f49ab9d5443bdf9285c23656e85e8b1fe095504
LABRPZ <47435318+LABRPZ@users.noreply.github.com>
2023-07-12 11:56:13 -0600

    Actualizacion
    -Se edita el excel del export
    Agregando columnas como text y dejando otras como general
    -Correxiones para funcionar en calidad

--- app/Exports/PlantillaExport.php
+++ app/Exports/PlantillaExport.php
@@ -6,8 +6,11 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithColumnWidths;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class PlantillaExport implements FromCollection, ShouldAutoSize, WithHeadings, WithColumnWidths
+class PlantillaExport implements FromCollection, ShouldAutoSize, WithHeadings, WithColumnWidths,WithColumnFormatting
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -32,5 +35,41 @@ class PlantillaExport implements FromCollection, ShouldAutoSize, WithHeadings, W
         return [
  
         ];
+
+        
     }
+        public function columnFormats(): array{
+        return [
+            'A' =>  NumberFormat::FORMAT_TEXT,
+            'B' => NumberFormat::FORMAT_TEXT,
+            'C' => NumberFormat::FORMAT_TEXT,
+            'D' =>  NumberFormat::FORMAT_TEXT,
+            'E' => NumberFormat::FORMAT_TEXT,
+            'F' => NumberFormat::FORMAT_TEXT,
+            'G' =>  NumberFormat::FORMAT_TEXT,
+            'H' => NumberFormat::FORMAT_TEXT,
+            'I' => NumberFormat::FORMAT_TEXT,
+            'J' =>  NumberFormat::FORMAT_TEXT,
+            'K' => NumberFormat::FORMAT_TEXT,
+            'L' => NumberFormat::FORMAT_TEXT,
+            'M' =>  NumberFormat::FORMAT_TEXT,
+            'N' => NumberFormat::FORMAT_TEXT,
+            'O' => NumberFormat::FORMAT_TEXT,
+            'P' =>  NumberFormat::FORMAT_TEXT,
+            'Q' => NumberFormat::FORMAT_TEXT,
+            'R' => NumberFormat::FORMAT_TEXT,
+            'S' => NumberFormat::FORMAT_TEXT,
+            'T' =>  NumberFormat::FORMAT_TEXT,
+            'U' => NumberFormat::FORMAT_TEXT,
+            'V' => NumberFormat::FORMAT_TEXT,
+            'W' =>  NumberFormat::FORMAT_TEXT,
+            'X' => NumberFormat::FORMAT_TEXT,
+            'Y' => NumberFormat::FORMAT_TEXT,
+            'Z' => NumberFormat::FORMAT_TEXT,
+            'AA' => NumberFormat::FORMAT_TEXT,
+
+        ];
+    }
+
+
 }
