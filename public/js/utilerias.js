ca8f9667ffc5367138729394db12d8ca1a95661e
ritchs <ricardohc03@gmail.com>
2023-08-01 17:53:51 -0600

    update logout

--- public/js/utilerias.js
+++ public/js/utilerias.js
@@ -12,6 +12,40 @@ var _g = {
 };
 //Configuracion de Notificaciones
 var _gen = {
+    block: function () {
+        const local = '127.0.0.1';
+        if (window.location.hostname != local) {
+            window.onload = function() {
+            document.addEventListener("contextmenu", function(e) {
+                e.preventDefault();
+            }, false);
+        }
+        }
+    },
+    logOut: function () {
+        Swal.fire({
+            title: '¿Estás seguro de que quieres cerrar la sesión?',
+            showDenyButton: true,
+            confirmButtonText: 'Aceptar',
+            denyButtonText: `Cancelar`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: '/logout',
+                    type: 'POST',
+                    data: {
+                      action: 'logout'
+                    }, // Line A
+                    success: function() {
+                        console.log("Cerrando Session");
+                        window.location.href = "/login";
+                    }
+                  });
+            }
+          })
+        
+    },
     notificacion: function (titulo, content, style) {
         if (style == 1) {
             var _c = '#739E73',
