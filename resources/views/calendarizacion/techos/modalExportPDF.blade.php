ba003e4c7f0d73acbddc13badaa7f78a72929301
Brian Dilan Garcia Paredes <60791442+DilanParedes99@users.noreply.github.com>
2023-08-28 11:57:51 -0600

    Correcion de modales para exportar techos financieros a EXCEL & PDF

--- resources/views/calendarizacion/techos/modalExportPDF.blade.php
+++ resources/views/calendarizacion/techos/modalExportPDF.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade export-pdf" id="exportPDF" tabindex="-1" role="dialog"
+<div class="modal fade export-pdf" id="TechosExportPDF" tabindex="-1" role="dialog"
      aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-md" role="document">
         <div class="modal-content">
