e24e87911f8003cc9dd5553f43694679eb53ff5e
Jeffer48 <herrerajeffer@gmail.com>
2023-10-31 13:52:26 -0600

    epp tipo_presupuesto

--- database/migrations/2023_05_31_194306_fondos_db.php
+++ database/migrations/2023_05_31_194306_fondos_db.php
@@ -612,6 +612,7 @@ return new class extends Migration
             $table->boolean('presupuestable')->default(false);
             $table->tinyInteger('con_mir')->nullable(false);
             $table->boolean('confirmado')->default(false);
+            $table->tinyInteger('tipo_presupuesto')->nullable(true);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->softDeletes();
