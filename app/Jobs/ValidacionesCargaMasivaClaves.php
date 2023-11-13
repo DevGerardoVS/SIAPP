9ef8e0b9eda48be70c5a37b5ed64d289ffe9e06d
LABRPZ <47435318+LABRPZ@users.noreply.github.com>
2023-11-13 13:25:10 -0600

    Update ValidacionesCargaMasivaClaves.php

--- app/Jobs/ValidacionesCargaMasivaClaves.php
+++ app/Jobs/ValidacionesCargaMasivaClaves.php
@@ -388,7 +388,7 @@ class ValidacionesCargaMasivaClaves implements ShouldQueue
 
                         if ($valuepresupuesto != $value) {
 
-                            array_push($arrayErrores, 'El total presupuestado  no es igual al techo financiero en la upp: ' . $arraysplit[0] . ' fondo: ' . $arraysplit[2] . ' ');
+                            array_push($arrayErrores, 'El total presupuestado  no es igual al techo financiero en la upp: ' . $arraysplit[0] . ' fondo: ' . $arraysplit[2] . ' Esperado: '.$valuepresupuesto.' dado: '.$value);
                         }
 
                         if ($VerifyEjercicio < 1) {
