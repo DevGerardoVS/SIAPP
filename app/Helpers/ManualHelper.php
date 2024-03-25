ed2d6932ee7fde4d9d4091c56fea723476d91aad
AngelTorres96 <42782811+AngelTorres96@users.noreply.github.com>
2024-03-25 16:30:45 -0600

    bitacora manuales

--- app/Helpers/ManualHelper.php
+++ app/Helpers/ManualHelper.php
@@ -34,6 +34,7 @@ class ManualHelper{
                     'usuario_modificacion' => $manual->usuario_modificacion,
                     'created_at'=>date("d/m/Y H:i:s", strtotime($manual->created_at)),
                     'updated_at'=>date("d/m/Y H:i:s", strtotime($manual->updated_at)),
+                    'id_sistema'=> $manual->id_sistema
                 );
                 
                 $manual->usuario_modificacion = Auth::user()->username;
@@ -58,6 +59,7 @@ class ManualHelper{
                 'usuario_modificacion' => $manual->usuario_modificacion,
                 'created_at'=>date("d/m/Y H:i:s", strtotime($manual->created_at)),
                 'updated_at'=>date("d/m/Y H:i:s", strtotime($manual->updated_at)),
+                'id_sistema'=> $manual->id_sistema
             );
             // generamos arreglo con el dato anterior y el nuevo
             $array_data_act = array(
@@ -94,6 +96,7 @@ class ManualHelper{
                     'usuario_modificacion' => $manual->usuario_modificacion,
                     'created_at'=>date("d/m/Y H:i:s", strtotime($manual->created_at)),
                     'updated_at'=>date("d/m/Y H:i:s", strtotime($manual->updated_at)),
+                    'id_sistema'=> $manual->id_sistema
                 );
                 
                 $manual->usuario_modificacion = Auth::user()->username;
@@ -110,6 +113,7 @@ class ManualHelper{
                 'usuario_modificacion' => $manual->usuario_modificacion,
                 'created_at'=>date("d/m/Y H:i:s", strtotime($manual->created_at)),
                 'updated_at'=>date("d/m/Y H:i:s", strtotime($manual->updated_at)),
+                'id_sistema'=> $manual->id_sistema
             );
             // generamos arreglo con el dato anterior y el nuevo
             $array_data_act = array(
