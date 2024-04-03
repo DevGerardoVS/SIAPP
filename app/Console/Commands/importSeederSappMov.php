93bad3223a329ce799b7d5ef075c9714827aac67
LABRPZ <47435318+LABRPZ@users.noreply.github.com>
2024-04-03 13:41:00 -0600

    Seeders de enero a marzo
    Para añadir un seeder hay que usar el comando y cambiar el archivo a seleccionar. Dicho archivo debe estar en storage/app revisar como cambiar el archivo en el archivo del comando

---
+++ app/Console/Commands/importSeederSappMov.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Imports\sapp_movimientos;
+use Illuminate\Console\Command;
+
+class importSeederSappMov extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:seederSappMov';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Importa a tu base local y genera un seeder para la tabla sapp_mov';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        //para funcionamento comodo de la importacion cambiar el nombre del archivo a importar aqui y en el archivo import spp_movimientos.php
+        $this->output->title('Starting import');
+        (new sapp_movimientos)->withOutput($this->output)->import('Marzo.xlsx');
+        $this->output->success('Import successful');
+    }
+}
