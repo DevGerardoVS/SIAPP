780857e1402017d4b5709faf629066b5bfb905d4
leoeltavo2 <leo.azul2@hotmail.com>
2024-04-11 12:14:11 -0600

    Create 2024_04_11_181316_desagregacion_mml_mir.php

---
+++ database/migrations/2024_04_11_181316_desagregacion_mml_mir.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('mml_mir', 'desagregacion_geografica')) {
+            Schema::table('mml_mir', function (Blueprint $table) {
+                $table->integer('desagregacion_geografica')->nullable();
+            });        
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mml_mir', function (Blueprint $table) {
+            $table->dropColumn('desagregacion_geografica');
+        });
+    }
+};
