8a31411f29acd64811a4792f337783aba5895640
Jeffer48 <herrerajeffer@gmail.com>
2024-06-24 15:18:35 -0600

    Tabla observaciones y procedimientos temporales
    Tabla de observaciones para claves de epp agregada,
    Procedimientos de reportes temporales, en lo que se agrega el nuevo epp

---
+++ database/migrations/2024_06_18_155304_epp_observaciones.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('epp_observaciones')) {
+            Schema::table('epp_observaciones', function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('id_epp');
+                $table->integer('etapa');
+                $table->string('observacion',500);
+                $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+                $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+                $table->softDeletes();
+                $table->string('created_user', 45);
+                $table->string('updated_user', 45)->nullable();
+                $table->string('deleted_user', 45)->nullable();
+
+                $table->foreign('id_epp')->references('id')->on('epp');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('epp_observaciones');
+    }
+};
