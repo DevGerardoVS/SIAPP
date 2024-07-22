35e9bfee977670c6f72fcff03b0fe325e67244d8
ritchs <ricardohc03@gmail.com>
2024-07-22 10:01:49 -0600

    add nuevo epp

--- database/migrations/2024_07_18_190837_correcciones_b_d.php
+++ database/migrations/2024_07_18_190837_correcciones_b_d.php
@@ -4,12 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use App\Models\Catalogo;
 use App\Models\Beneficiarios;
 use App\Models\UnidadesMedida;
-use App\Models\catalogos\EntidadEjecutora;
-use App\Models\Epp;
-use App\Models\catalogos\ClasificacionAdministrativa;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-use App\Models\UppExtras;
+
 
 return new class extends Migration
 {
@@ -23,9 +18,7 @@ return new class extends Migration
     {
      
         try {
-          //  DB::beginTransaction();
-            DB::select("CALL rellenar_tablas_intermedias(?)",["SISTEMA"]);
-            DB::select("CALL llenar_v_epp(?,?)",[null,"SISTEMA"]);
+          DB::beginTransaction();
 
             DB::unprepared("UPDATE mml_actividades SET id_catalogo =NULL WHERE id in(1188,1191,1192,1198,1199,1200,1229,1230,1231,1232,1233,1234,1235);");
             $cat = Catalogo::where(['clave' => "UUU", 'ejercicio' => 2024,'grupo_id'=>'ACTIVIDADES ADMON'])->first();
@@ -39,38 +32,13 @@ return new class extends Migration
 
             $ben = Beneficiarios::where('beneficiario',"Personas")->first();
             $uni = UnidadesMedida::where('unidad_medida', "Pago")->first();
-            if(isset($ben) && isset( $uni)){
+            if (isset($ben) && isset($uni)) {
                 DB::unprepared("UPDATE metas SET beneficiario_id =   $ben->id, unidad_medida_id = $uni->id, cantidad_beneficiarios = 1, enero = 0, febrero = 0, marzo = 0, abril = 0, mayo = 0, junio = 1, julio = 0, agosto = 0, septiembre = 0, octubre = 0, noviembre = 0, diciembre = 0, total = 1 WHERE id in(4305,4306);");
             }
-            $clf_a = ClasificacionAdministrativa::where('deleted_at', null);
-            foreach ($clf_a as $key ) {
-                $rel = UppExtras::where('clasificacion_administrativa_id ', $key->id)->first();
-                $editclf = ClasificacionAdministrativa::find($key->id);
-                if (count($rel) >= 1) {
-                    $editclf->estatus = 1;
-                    $editclf->save();
-                }
-            }
 
-            $ej = EntidadEjecutora::select('id')->get();
-            foreach ($ej as $key) {
-                $epp = Epp::where('entidad_ejecutora_id', $key->id)->get();
-                $editEj = EntidadEjecutora::find($key->id);
-                //cambiar estatus de entidad ejecutora
-                if (count($epp) >= 1) {
-                    $editEj->estatus = 4;
-                    $editEj->save();
-                    //cambiar estatus de entidad ejecutora
-
-                } else {
-                    $editEj->estatus = 3;
-                    $editEj->save();
-                }
-            }
-
-            //DB::commit();
+            DB::commit();
         } catch (\Throwable $th) {
-         //   DB::rollBack();
+            DB::rollBack();
 			throw $th;
         }
     }
