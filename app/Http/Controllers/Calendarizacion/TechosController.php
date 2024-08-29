99e57fe3567f594835dc615a9916c753a1be90b1
Brian Dilan Garcia Paredes <60791442+DilanParedes99@users.noreply.github.com>
2024-08-29 09:46:17 -0600

    FIX -> Techos financieros ya edita

--- app/Http/Controllers/Calendarizacion/TechosController.php
+++ app/Http/Controllers/Calendarizacion/TechosController.php
@@ -114,17 +114,30 @@ class TechosController extends Controller
             ->limit(1)
             ->get();
 
-        $data = DB::table('techos_financieros as tf')
+        $prueba = DB::table('techos_financieros as tf')
+            ->select('tf.id','tf.clv_upp','ve.upp as descPre','tf.tipo','tf.clv_fondo','c.descripcion as fondo_ramo','tf.presupuesto','tf.ejercicio')
+            ->leftJoin('catalogo as c','tf.clv_fondo','=','c.clave')
+            ->leftJoin('v_epp as ve','tf.clv_upp','=','ve.clv_upp')
+            ->where('tf.deleted_at','=',null)
+            ->where('tf.ejercicio','=',intval($max_ejercicio[0]->ejercicio))
+            ->where('ve.ejercicio','=',intval($max_ejercicio[0]->ejercicio))
+            ->where('c.grupo_id','=','FONDO DEL RAMO')
+            ->where('tf.id','=',$request->id)
+            ->distinct()
+            ->get();
+        log::debug($prueba);
+
+        /* $data = DB::table('techos_financieros as tf')
             ->select('tf.id','tf.clv_upp','vee.upp as descPre','tf.tipo','tf.clv_fondo','f.fondo_ramo','tf.presupuesto','tf.ejercicio')
             ->leftJoinSub('select distinct clv_upp, upp, ejercicio as Ej from v_epp','vee','tf.clv_upp','=','vee.clv_upp')
             ->leftJoinSub('select distinct clv_fondo_ramo, fondo_ramo from fondo','f','tf.clv_fondo','=','f.clv_fondo_ramo')
             ->where('tf.id','=',$request->id)
             ->where('tf.ejercicio','=',$max_ejercicio[0]->ejercicio)
             ->where('vee.Ej','=',$max_ejercicio[0]->ejercicio)
-            ->get();
+        ->get(); */
 
             return [
-                'data' => $data
+                'data' => $prueba
             ];
     }
 
