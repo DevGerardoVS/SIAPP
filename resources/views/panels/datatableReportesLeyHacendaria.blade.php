dee5ba40abd6e0252574db4c30fd170b9e29f6ba
leoeltavo2 <leo.azul2@hotmail.com>
2024-08-29 17:03:49 -0600

    Corrección reportes
    *Se corrigieron los reportes para que cambien de acuerdo al año (solo falta modificar a que sean 2024 en lugar de 2023).
    *Se modificó la parte de la tabla para que vaya cambiando de acuerdo al año que se busca.

--- resources/views/panels/datatableReportesLeyHacendaria.blade.php
+++ resources/views/panels/datatableReportesLeyHacendaria.blade.php
@@ -21,8 +21,8 @@
                             var getCorrectDate = new Date(date.valueOf() + date.getTimezoneOffset() * 60000);
                             var formattedDate = ("0" + getCorrectDate.getDate()).slice(-2) + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + date.getFullYear();
 
-                            if(data[i].version == 0) par.append(new Option("Última versión", data[i].deleted_at));
-                            else par.append(new Option("V"+ data[i].version +" - "+formattedDate , data[i].deleted_at));
+                            if(data[i].version == 0) par.append(new Option("Última versión", data[i].version+"_"+data[i].deleted_at));
+                            else par.append(new Option("V"+ data[i].version +" - "+formattedDate , data[i].version+"_"+data[i].deleted_at));
                         });
                     }
                 });
@@ -34,46 +34,72 @@
                     success: function(data) {
                         $("#putNames").html('');
                         let csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-                        data.forEach(function (row,index) {
-                            
+
+                        let newData = JSON.parse(JSON.stringify(data)); // Copia de data para modificar los nombres.
+                        newData[3].name = "reporte_art_20_frac_X_a_num_3";
+                        newData[4].name = "reporte_art_20_frac_X_a_num_4";
+                        newData[5].name = "reporte_art_20_frac_X_a_num_5";
+                        newData[6].name = "reporte_art_20_frac_X_a_num_1";
+                        newData[7].name = "reporte_art_20_frac_X_a_num_2";
+                        newData[8].name = "reporte_art_20_frac_X_b_num_3";
+                        newData[17].name = "reporte_art_20_frac_X_b_num_4";
+                        newData[18].name = "reporte_art_20_frac_X_b_num_2";
+                        newData[9].name = "reporte_art_20_frac_X_c_num_3";
+                        newData[10].name = "reporte_art_20_frac_X_c_num_4_1";
+                        newData[11].name = "reporte_art_20_frac_X_c_num_4_2";
+                        newData[12].name = "reporte_art_20_frac_X_c_num_4_3";
+                        newData[13].name = "reporte_art_20_frac_X_c_num_4_4";
+                        newData[14].name = "reporte_art_20_frac_X_c_num_4_6";
+                        newData[15].name = "reporte_art_20_frac_X_c_num_4_7";
+                        newData[16].name = "reporte_art_20_frac_X_c_num_4_8";
+
+                        let getDataToIterate = anio > 2023 ? newData : data;
+
+                        function extractNumber(str) {
+                            const match = str.match(/\d+/g);
+                            return match ? parseInt(match.join('')) : 0;
+                        }
+
+                        function compareNames(a, b) {
+                            // Extrae las partes numéricas del texto.
+                            const nameA = a.name.replace(/[^\d]+/g, '');
+                            const nameB = b.name.replace(/[^\d]+/g, '');
+
+                            const numberA = extractNumber(nameA);
+                            const numberB = extractNumber(nameB);
+
+                            // Comparar el texto.
+                            const textA = a.name.replace(/\d+/g, '');
+                            const textB = b.name.replace(/\d+/g, '');
+
+                            if (textA < textB) return -1;
+                            if (textA > textB) return 1;
+
+                            // Compara las partes numericas si el texto es similar.
+                            if (numberA < numberB) return -1;
+                            if (numberA > numberB) return 1;
+
+                            return 0;
+                        }
+                        getDataToIterate.sort(compareNames);
+
+                        getDataToIterate.forEach(function (row,index) {
                             let replaceUnderscore = row.name.replace(/_/g, ' ');
                             let replaceReport = replaceUnderscore.replace(/reporte/g, '');
                             let replaceNum = replaceReport.replace(/num/g, 'numeral');
                             if (replaceNum.includes('num')) {
                                 replaceNum = replaceNum.slice(0, 15) + 'inc ' + replaceNum.slice(15);
                             }
-                            let replaceDot = replaceNum.replace(/11 /g, '11.');
+                            let replaceDot =  anio > 2023 ? replaceNum.replace(/4 /g, '4.') : replaceNum.replace(/11 /g, '11.');
                             let correctName = replaceDot;
-                            let newName = "";
-                            if(anio > 2023){
-                                if(correctName == " art 20 frac X inc a numeral 3") newName = " art 20 frac X inc a numeral 5";
-                                if(correctName == " art 20 frac X inc a numeral 1") newName = " art 20 frac X inc a numeral 3";
-                                if(correctName == " art 20 frac X inc a numeral 2") newName = " art 20 frac X inc a numeral 4"; //pendiente de revisión
-                                if(correctName == " art 20 frac X inc b numeral 3") newName = " art 20 frac X inc b numeral 4";
-                                if(correctName == " art 20 frac X inc a numeral 6") newName = " art 20 frac X inc a numeral 2";
-                                if(correctName == " art 20 frac X inc b numeral 4") newName = " art 20 frac X inc b numeral 2";
-                                if(correctName == " art 20 frac X inc b numeral 1") newName = " art 20 frac X inc b numeral 3";
-                                if(correctName == " art 20 frac X inc b numeral 10") newName = " art 20 frac X inc c numeral 3";
-                                if(correctName == " art 20 frac X inc b numeral 11.1") newName = " art 20 frac X inc c numeral 4.1";
-                                if(correctName == " art 20 frac X inc b numeral 11.2") newName = " art 20 frac X inc c numeral 4.2";
-                                if(correctName == " art 20 frac X inc b numeral 11.3") newName = " art 20 frac X inc c numeral 4.3";
-                                if(correctName == " art 20 frac X inc b numeral 11.4") newName = " art 20 frac X inc c numeral 4.4";
-                                if(correctName == " art 20 frac X inc b numeral 11.6") newName = " art 20 frac X inc c numeral 4.6";
-                                if(correctName == " art 20 frac X inc b numeral 11.7") newName = " art 20 frac X inc c numeral 4.7";
-                                if(correctName == " art 20 frac X inc b numeral 11.8") newName = " art 20 frac X inc c numeral 4.8";
-                                if(correctName == " art 20 frac II") newName = " art 20 frac II";
-                                if(correctName == " art 20 frac III") newName = " art 20 frac III";
-                                if(correctName == " art 20 frac IX") newName = " art 20 frac IX";
-                            }
                             var newRow = `
                                 <tr>
                                     <td class="d-flex justify-content-between px-5">
-                                        <div class="my-auto me-2" style="font-size: 14px;">${anio >2023 ? newName.toUpperCase(): correctName.toUpperCase()}</div>
+                                        <div class="my-auto me-2" style="font-size: 14px;">${correctName.toUpperCase()}</div>
                                         <div class="d-flex justify-content-end flex-wrap p-1">
                                             <form action="/Reportes/download/${row.name}" method="POST" enctype="multipart/form-data" class="my-auto">
                                                 <input type="hidden" name="_token" value=${csrfToken}>
-                                                <input type="text" hidden class="anio" id="anio" name="anio">
-                                                <input type="text" hidden class="anio" id="anio" name="anio">
+                                                <input type="text" hidden class="anio" id="anio" name="anio" value=${anio}>
                                                 <input type="text" hidden class="fechaCorte" id="fechaCorte" name="fechaCorte">
                                                 <button id="btnPDF" type="submit" formtarget="_blank" class="btn btn-light btn-sm btn-labeled me-sm-3 align-middle" style="border-color: #6a0f49;" title="Generar Reporte PDF" name="action" value="pdf">
                                                     <span class="btn-label"><i class="fa fa-file-pdf-o text-danger fs-4 align-middle"></i></span>
@@ -118,7 +144,9 @@
                 }
             });
 
-            $(".anio").val($('#anio_filter option:selected').val());
+            // setTimeout(() => {
+            //     $(".anio").val($('#anio_filter option:selected').val());
+            // }, 800);
             getDataFechaCorte($('#anio_filter option:selected').val());
             getNames($('#anio_filter option:selected').val());
 
@@ -136,8 +164,9 @@
             $("#buscarForm").on("change", ".filters_anio", function(e) {
                 e.preventDefault();
                 $("#fechaCorte_filter").val("");
-                $(".anio").val($('#anio_filter').val());
                 setTimeout(() => {
+                    // console.log($("#anio").val());
+                    // $(".anio").val($('#anio_filter').val());
                     $(".fechaCorte").val($('#fechaCorte_filter').val());
                 }, 500);
                 getDataFechaCorte($('#anio_filter').val());
