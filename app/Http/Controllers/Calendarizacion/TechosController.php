c6ede79f33e11bf19dafb0567cd0aafdaa68044e
Brian Dilan Garcia Paredes <60791442+DilanParedes99@users.noreply.github.com>
2024-08-29 10:36:47 -0600

    FIX -> exports de techos financieros

--- app/Http/Controllers/Calendarizacion/TechosController.php
+++ app/Http/Controllers/Calendarizacion/TechosController.php
@@ -40,23 +40,23 @@ class TechosController extends Controller
     public function getTechos(Request $request){
         Controller::check_permission('getTechos');
         $dataSet = [];
-        $prueba = DB::table('techos_financieros as tf')
+        $data = DB::table('techos_financieros as tf')
             ->select('tf.id','tf.clv_upp','ve.upp as descPre','tf.tipo','tf.clv_fondo','c.descripcion as fondo_ramo','tf.presupuesto','tf.ejercicio','tf.updated_user')
             ->leftJoin('catalogo as c','tf.clv_fondo','=','c.clave')
             ->leftJoin('v_epp as ve','tf.clv_upp','=','ve.clv_upp')
             ->where('tf.deleted_at','=',null);
             if($request->anio_filter != null){
-                $prueba =  $prueba -> where('tf.ejercicio','=',intval($request->anio_filter));
-                $prueba =  $prueba -> where('ve.ejercicio','=',intval($request->anio_filter));
+                $data =  $data -> where('tf.ejercicio','=',intval($request->anio_filter));
+                $data =  $data -> where('ve.ejercicio','=',intval($request->anio_filter));
             }
             if($request->fondo_filter != null && $request->fondo_filter != '0'){
-                $prueba = $prueba -> where('tf.clv_fondo','=',$request->fondo_filter);
+                $data = $data -> where('tf.clv_fondo','=',$request->fondo_filter);
             }
             if($request->upp_filter != null && $request->upp_filter != '0'){
-                $prueba = $prueba -> where('tf.clv_upp','=',$request->upp_filter);
+                $data = $data -> where('tf.clv_upp','=',$request->upp_filter);
             }
 
-        $prueba = $prueba 
+        $data = $data 
             ->where('c.grupo_id','=','FONDO DEL RAMO')
             ->orderBy('tf.clv_upp','asc')
             ->distinct()
@@ -87,7 +87,7 @@ class TechosController extends Controller
             ->get();
 
         
-            foreach ($prueba as $d){
+            foreach ($data as $d){
             if($max_ejercicio[0]->ejercicio == $d->ejercicio){
                 $button2 = '<a class="btn btn-secondary" onclick="getEdita('.$d->id.')" data-bs-toggle="modal" data-bs-target="#editar" ><i class="fa fa-pencil" style="font-size: large; color: white"></i></a>';
                 /* $button3 = '<button id="eliminar" title="Eliminar" class="btn btn-danger"><i class="fa fa-trash" style="font-size: large"></i></button>'; */
@@ -101,7 +101,7 @@ class TechosController extends Controller
 
         return [
             'dataSet'=>$dataSet,
-            'data' => json_encode($prueba)
+            'data' => json_encode($data)
         ];
     }
 
@@ -114,7 +114,7 @@ class TechosController extends Controller
             ->limit(1)
             ->get();
 
-        $prueba = DB::table('techos_financieros as tf')
+        $data = DB::table('techos_financieros as tf')
             ->select('tf.id','tf.clv_upp','ve.upp as descPre','tf.tipo','tf.clv_fondo','c.descripcion as fondo_ramo','tf.presupuesto','tf.ejercicio')
             ->leftJoin('catalogo as c','tf.clv_fondo','=','c.clave')
             ->leftJoin('v_epp as ve','tf.clv_upp','=','ve.clv_upp')
@@ -125,7 +125,6 @@ class TechosController extends Controller
             ->where('tf.id','=',$request->id)
             ->distinct()
             ->get();
-        log::debug($prueba);
 
         /* $data = DB::table('techos_financieros as tf')
             ->select('tf.id','tf.clv_upp','vee.upp as descPre','tf.tipo','tf.clv_fondo','f.fondo_ramo','tf.presupuesto','tf.ejercicio')
@@ -137,7 +136,7 @@ class TechosController extends Controller
         ->get(); */
 
             return [
-                'data' => $prueba
+                'data' => $data
             ];
     }
 
